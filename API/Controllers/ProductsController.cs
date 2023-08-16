4d32ffd429dcaea5bd5d0dfcc1c006937f91095b
YoussefSS <joesamir98@gmail.com>
2023-08-16 12:02:30 +0300

    34. Implementing the methods in the Generic repository

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,17 +10,24 @@ namespace API.Controllers
     [Route("api/[controller]")] // route will be /api/products/
     public class ProductsController : ControllerBase
     {
-        public IProductRepository _repo;
 
-        public ProductsController(IProductRepository repo)
+        private readonly IGenericRepository<Product> _productsRepo;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepo;
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
+
+        public ProductsController(IGenericRepository<Product> productsRepo,
+            IGenericRepository<ProductBrand> productBrandRepo,
+            IGenericRepository<ProductType> productTypeRepo)
         {
-            _repo = repo;
+            _productsRepo = productsRepo;
+            _productTypeRepo = productTypeRepo;
+            _productBrandRepo = productBrandRepo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _repo.GetProductsAsync();
+            var products = await _productsRepo.ListAllAsync();
             return Ok(products);
         }
 
@@ -30,19 +37,19 @@ namespace API.Controllers
         [HttpGet("{id}")] // route will be /api/products/IDHERE
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _repo.GetProductByIdAsync(id); // takes in a primary key
+            return await _productsRepo.GetByIdAsync(id); // takes in a primary key
         }
 
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
-            return Ok(await _repo.GetProductBrandsAsync()); // asp.net does not allow us to directly return an IReadOnlyList, so we must wrap it in Ok
+            return Ok(await _productBrandRepo.ListAllAsync()); // asp.net does not allow us to directly return an IReadOnlyList, so we must wrap it in Ok
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
-            return Ok(await _repo.GetProductTypesAsync());
+            return Ok(await _productTypeRepo.ListAllAsync());
         }
     }
 }
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,18 +1,28 @@
+using System.Security;
 using Core.Entities;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity // We need to match the constraints here
     {
-        public Task<T> GetByIdAsync(int id)
+        private readonly StoreContext _context;
+        public GenericRepository(StoreContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<IReadOnlyList<T>> ListAllAsync()
+        public async Task<T> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            // Set creates a .DbSet around the T type which we can use to query
+            // We cant use .Include() here as we don't know what type T is
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
         }
     }
 }

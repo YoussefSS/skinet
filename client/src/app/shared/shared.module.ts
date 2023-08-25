7a73e9484dc32bb1f2a3c7b8a62e75d27d8c91fd
YoussefSS <joesamir98@gmail.com>
2023-08-25 20:19:31 +0300

    102. Pagination and Output properties

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './paging-header/paging-header.component';
+import { PagerComponent } from './pager/pager.component';
 
 @NgModule({
-  declarations: [PagingHeaderComponent],
+  declarations: [PagingHeaderComponent, PagerComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(), // forRoot so it's loaded as a singleton and the same module is used throughout the app
   ],
-  exports: [PaginationModule, PagingHeaderComponent],
+  exports: [PaginationModule, PagingHeaderComponent, PagerComponent],
 })
 export class SharedModule {}

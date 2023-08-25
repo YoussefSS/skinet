cfa276106ccd479ff5574cf27e24b080098c33b9
YoussefSS <joesamir98@gmail.com>
2023-08-25 20:10:59 +0300

    101. Making the pagination component a shared component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './paging-header/paging-header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PagingHeaderComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(), // forRoot so it's loaded as a singleton and the same module is used throughout the app
   ],
-  exports: [PaginationModule],
+  exports: [PaginationModule, PagingHeaderComponent],
 })
 export class SharedModule {}

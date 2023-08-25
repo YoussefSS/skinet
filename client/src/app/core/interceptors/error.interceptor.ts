91b66227e207d139b6818489be31e7bb5967f9b6
YoussefSS <joesamir98@gmail.com>
2023-08-26 00:15:07 +0300

    118. Using the Http Interceptor to catch errors

---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  // injecting the router because we'll navigate users that get the error into our error components
+  constructor(private router: Router) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    // handle returns an Observable<HttpEvent<any>>.. We'll use rxJS to handle this which is the pipe method
+    // In the pipe method we can do anything to an observable before we pass it onto our other components
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error) {
+          if (error.status === 404) {
+            this.router.navigateByUrl('/not-found');
+          }
+
+          if (error.status === 500) {
+            this.router.navigateByUrl('/server-error');
+          }
+        }
+        return throwError(() => new Error(error.message));
+      })
+    );
+  }
+}

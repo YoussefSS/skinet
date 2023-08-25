e59bbc7f181179dca428b072ba0cda16c1a81593
YoussefSS <joesamir98@gmail.com>
2023-08-25 18:18:06 +0300

    97. Adding the pagination functionality

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    PaginationModule.forRoot(), // forRoot so it's loaded as a singleton and the same module is used throughout the app
+  ],
+  exports: [PaginationModule],
 })
-export class SharedModule { }
+export class SharedModule {}

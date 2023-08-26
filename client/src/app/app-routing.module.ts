8230e8da4c9407286e218846883e62e7d2435c28
YoussefSS <joesamir98@gmail.com>
2023-08-26 12:52:46 +0300

    125. Adding breadcrumbs

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,11 +6,14 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
   { path: 'test-error', component: TestErrorComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: 'server-error', component: ServerErrorComponent },
-  { path: 'shop', loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule) }, // this route will be lazily loaded when we go to the '/shop' path
+  {
+    path: 'shop',
+    loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule),
+  }, // this route will be lazily loaded when we go to the '/shop' path
   { path: '**', redirectTo: '', pathMatch: 'full' }, // a route that does not exist
 ];
 

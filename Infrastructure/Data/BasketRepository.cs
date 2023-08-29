b5deb195d4c768562996c98e71142fdb2e8d1950
YoussefSS <joesamir98@gmail.com>
2023-08-29 11:52:32 +0300

    138. Implementing the basket repository

--- Infrastructure/Data/BasketRepository.cs
+++ Infrastructure/Data/BasketRepository.cs
@@ -1,27 +1,42 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.Json;
 using Core.Entities;
 using Core.Interfaces;
+using StackExchange.Redis;
 
 namespace Infrastructure.Data
 {
     public class BasketRepository : IBasketRepository
     {
-        public Task<bool> DeleteBasketAsync(string basketId)
+        private readonly IDatabase _database;
+        public BasketRepository(IConnectionMultiplexer redis)
         {
-            throw new NotImplementedException();
+            _database = redis.GetDatabase(); // now we've got a connection to our Redis database
         }
 
-        public Task<CustomerBasket> GetBasketAsync(string basketId)
+        public async Task<bool> DeleteBasketAsync(string basketId)
         {
-            throw new NotImplementedException();
+            return await _database.KeyDeleteAsync(basketId);
         }
 
-        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        // Getting the data from our Redis database as a JSON string then deserializing it into a CustomerBasket object
+        public async Task<CustomerBasket> GetBasketAsync(string basketId)
         {
-            throw new NotImplementedException();
+            var data = await _database.StringGetAsync(basketId);
+            return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(data);
+        }
+
+        // This updates (replaces) or creates a new customer basket
+        // If we are updating then we are going to replace the existing basket in our redis DB with a new version, so not actually updating individual values
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            var created = await _database.StringSetAsync(
+                basket.Id,
+                JsonSerializer.Serialize(basket),
+                TimeSpan.FromDays(30)); // 30 day lifespan
+
+            if (!created) return null;
+
+            return await GetBasketAsync(basket.Id);
         }
     }
 }

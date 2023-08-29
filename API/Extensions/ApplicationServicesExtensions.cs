abb5fcda0f542501b229258707f1b7ec56e8dfd1
YoussefSS <joesamir98@gmail.com>
2023-08-29 10:40:10 +0300

    135. Setting up Redis

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -3,6 +3,8 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
 
 namespace API.Extensions
 {
@@ -16,6 +18,11 @@ namespace API.Extensions
             services.AddDbContext<StoreContext>(opt =>
             {
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
             });
             services.AddScoped<IProductRepository, ProductRepository>(); // AddScoped means its lifetime is the scope of the HTTP request
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));

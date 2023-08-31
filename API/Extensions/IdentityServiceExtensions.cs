417722bc77d868c247a61f497327b60b68236ea2
YoussefSS <joesamir98@gmail.com>
2023-08-31 22:52:15 +0300

    167. Adding the Startup services for identity

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,6 @@
+using Core.Entities.Identity;
 using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -11,6 +13,17 @@ namespace API.Extensions
             {
                 opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
             });
+
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                // can change identity options like password requirements, Lockout password attempts, etc..
+            })
+            .AddEntityFrameworkStores<AppIdentityDbContext>() // required to store our users in a database (EF core)
+            .AddSignInManager<SignInManager<AppUser>>(); // we'll use this for signing in
+
+            // Authentication always comes before authorization as we can't authorize someone to do something without authenticating them
+            services.AddAuthentication();
+            services.AddAuthorization();
 
             return services;
         }

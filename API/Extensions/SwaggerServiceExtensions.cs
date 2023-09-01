0eb6c57a7016b39ccf40ff91fe6225b027106d94
YoussefSS <joesamir98@gmail.com>
2023-09-01 23:28:11 +0300

    184. Updating swagger config for identity

---
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,51 @@
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(config =>
+            {
+                // The following is some boilerplate to show when an endpoint requires authorization
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header, // Where do we want this authorization bearer to be
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                config.AddSecurityDefinition("Bearer", securitySchema);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    {
+                        securitySchema, new[] {"Bearer"}
+                    }
+                };
+
+                config.AddSecurityRequirement(securityRequirement);
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            return app;
+        }
+    }
+}

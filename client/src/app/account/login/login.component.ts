161ef1ac6c76e6035a1c3959a6f3e1d6b4af1d11
YoussefSS <joesamir98@gmail.com>
2023-09-02 13:05:16 +0300

    191. Using reactive forms in Angular

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    // Here we define our FormControls
+    email: new FormControl('', Validators.required), // initial value in parenthesis
+    password: new FormControl('', Validators.required),
+  });
 
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { PagingHeaderComponent } from './paging-header/paging-header.component';
 import { PagerComponent } from './pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
@@ -12,6 +13,7 @@ import { OrderTotalsComponent } from './order-totals/order-totals.component';
     CommonModule,
     PaginationModule.forRoot(), // forRoot so it's loaded as a singleton and the same module is used throughout the app
     CarouselModule.forRoot(),
+    ReactiveFormsModule,
   ],
   exports: [
     PaginationModule,
@@ -19,6 +21,7 @@ import { OrderTotalsComponent } from './order-totals/order-totals.component';
     PagerComponent,
     CarouselModule,
     OrderTotalsComponent,
+    ReactiveFormsModule,
   ],
 })
 export class SharedModule {}

50a26e4d6e85861de5d513774fe7505f4c6b4aec
YoussefSS <joesamir98@gmail.com>
2023-09-02 13:34:10 +0300

    194. Persisting the login

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../shared/models/user';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 // We'll be making calls to our API here
@@ -17,6 +17,20 @@ export class AccountService {
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient, private router: Router) {}
+
+  loadCurrentUser(token: string) {
+    // We'll send the token to our API so that we can authenticate them
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<User>(this.baseUrl + 'account', { headers }).pipe(
+      // We'll update the currentUser observable and set the token
+      map((user) => {
+        localStorage.setItem('token', user.token); // store the token in local storage so that it persists
+        this.currentUserSource.next(user); // store the user object inside the observable
+      })
+    );
+  }
 
   // Will receive values from a form
   login(values: any) {

918daa23f6be661de22eda04a9800afecee4d49a
YoussefSS <joesamir98@gmail.com>
2023-09-03 17:18:26 +0300

    208. Creating the order aggregate part 1

---
+++ Core/Entities/OrderAggregate/Address.cs
@@ -0,0 +1,27 @@
+namespace Core.Entities.OrderAggregate
+{
+    // This is going to be an 'owned' entity
+    public class Address
+    {
+        // Empty constructor is needed by EF
+        public Address()
+        {
+        }
+
+        public Address(string firstName, string lastName, string street, string city, string state, string zipcode)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Street = street;
+            City = city;
+            State = state;
+            Zipcode = zipcode;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+    }
+}

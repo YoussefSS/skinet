8631edaddc6852acb5b1536c71a65b31ddc2ba4c
YoussefSS <joesamir98@gmail.com>
2023-09-05 21:39:05 +0300

    243. Populating the address form from API

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss'],
 })
-export class CheckoutComponent {
-  constructor(private fb: FormBuilder) {}
+export class CheckoutComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAddressFormValues();
+  }
 
   // A parent form with nested child forms
   checkoutForm = this.fb.group({
@@ -17,13 +25,24 @@ export class CheckoutComponent {
       street: ['', Validators.required],
       city: ['', Validators.required],
       state: ['', Validators.required],
-      zipcode: ['', Validators.required],
+      zipCode: ['', Validators.required],
     }),
     deliveryForm: this.fb.group({
       deliveryMethod: ['', Validators.required],
     }),
-    paymentForm: this.fb.group({ // We'll look at taking CC payments later with stripe
+    paymentForm: this.fb.group({
+      // We'll look at taking CC payments later with stripe
       nameOnCard: ['', Validators.required],
     }),
   });
+
+  getAddressFormValues() {
+    this.accountService.getUserAddress().subscribe({
+      next: (address) => {
+        if (address) {
+          this.checkoutForm.get('addressForm')?.patchValue(address);
+        }
+      },
+    });
+  }
 }

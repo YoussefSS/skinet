fd788c1334dac062ddbce44b29768b763d58bef1
YoussefSS <joesamir98@gmail.com>
2023-09-05 16:17:31 +0300

    234. Adding the checkout form

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent {
+  constructor(private fb: FormBuilder) {}
 
+  // A parent form with nested child forms
+  checkoutForm = this.fb.group({
+    addressForm: this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipcode: ['', Validators.required],
+    }),
+    deliveryForm: this.fb.group({
+      deliveryMethod: ['', Validators.required],
+    }),
+    paymentForm: this.fb.group({ // We'll look at taking CC payments later with stripe
+      nameOnCard: ['', Validators.required],
+    }),
+  });
 }

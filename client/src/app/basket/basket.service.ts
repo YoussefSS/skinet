b12cc7d8307de8621f72fcab0d57156276dc2d64
YoussefSS <joesamir98@gmail.com>
2023-09-05 23:23:19 +0300

    245. Updating the shipping price

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -4,6 +4,7 @@ import { Basket, BasketItem, BasketTotals } from '../shared/models/basket';
 import { BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../shared/models/product';
+import { DeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +15,14 @@ export class BasketService {
   basketSource$ = this.basketSource.asObservable(); // our components can subscribe to this and be notified of changes and update accordingly
   private basketTotalSource = new BehaviorSubject<BasketTotals | null>(null);
   basketTotalSource$ = this.basketTotalSource.asObservable();
+  shipping = 0;
 
   constructor(private http: HttpClient) {}
+
+  setShippingPrice(deliveryMethod: DeliveryMethod) {
+    this.shipping = deliveryMethod.price;
+    this.calculateTotals();
+  }
 
   // Unlike previously where we would subscribe to the get methods, our other components will just use basketSource$
 
@@ -112,10 +119,9 @@ export class BasketService {
   private calculateTotals() {
     const basket = this.getCurrentBasketValue();
     if (!basket) return;
-    const shipping = 0;
     const subtotal = basket.items.reduce((a, b) => b.price * b.quantity + a, 0);
-    const total = subtotal + shipping;
-    this.basketTotalSource.next({ shipping, total, subtotal });
+    const total = subtotal + this.shipping;
+    this.basketTotalSource.next({ shipping: this.shipping, total, subtotal });
   }
 
   private isProduct(item: Product | BasketItem): item is Product {

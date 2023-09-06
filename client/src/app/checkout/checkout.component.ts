b0494313db8bc4e7a456c87184924a0c22530082
YoussefSS <joesamir98@gmail.com>
2023-09-06 14:14:25 +0300

    258. Updating the client basket

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../account/account.service';
+import { BasketService } from '../basket/basket.service';
 
 @Component({
   selector: 'app-checkout',
@@ -10,11 +11,13 @@ import { AccountService } from '../account/account.service';
 export class CheckoutComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
-    private accountService: AccountService
+    private accountService: AccountService,
+    private basketService: BasketService
   ) {}
 
   ngOnInit(): void {
     this.getAddressFormValues();
+    this.getDeliveryMethodValue();
   }
 
   // A parent form with nested child forms
@@ -44,5 +47,12 @@ export class CheckoutComponent implements OnInit {
         }
       },
     });
+  }
+
+  getDeliveryMethodValue() {
+    const basket = this.basketService.getCurrentBasketValue();
+    if(basket && basket.deliveryMethodId) {
+      this.checkoutForm.get('deliveryForm')?.get('deliveryMethod')?.patchValue(basket.deliveryMethodId.toString())
+    }
   }
 }

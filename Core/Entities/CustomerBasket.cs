2a4a9421e1f381fe6bbad65fff716e913da054d7
YoussefSS <joesamir98@gmail.com>
2023-09-06 13:30:46 +0300

    256. Implementing the payment intent

--- Core/Entities/CustomerBasket.cs
+++ Core/Entities/CustomerBasket.cs
@@ -13,5 +13,9 @@ namespace Core.Entities
 
         public string Id { get; set; } // customer/angular will generate the ID
         public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+        public int? DeliveryMethodId { get; set; } // Optional as the user can only select the delivery method during checkout
+        public string ClientSecret { get; set; } // Used by stripe to confirm payment intent
+        public string PaymentIntentId { get; set; } // We'll use this to update the payment intent if the client updates any info
+
     }
 }
--- Infrastructure/Services/PaymentService.cs
+++ Infrastructure/Services/PaymentService.cs
@@ -1,13 +1,85 @@
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using Stripe;
 
 namespace Infrastructure.Services
 {
     public class PaymentService : IPaymentService
     {
-        public Task<CustomerBasket> CreateOrUpdatePaymentIntent(string barketId)
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration _config;
+        private readonly IBasketRepository _basketRepository;
+        public PaymentService(IBasketRepository basketRepository, IUnitOfWork unitOfWork, IConfiguration config)
         {
-            throw new NotImplementedException();
+            _basketRepository = basketRepository;
+            _config = config;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            // This is our secret key - StripeConfiguration is a global config class
+            StripeConfiguration.ApiKey = _config["StripeSettings:SecretKey"];
+
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+            var shippingPrice = 0m;
+
+            if (basket.DeliveryMethodId.HasValue)
+            {
+                var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>()
+                    .GetByIdAsync((int)basket.DeliveryMethodId);
+                shippingPrice = deliveryMethod.Price;
+            }
+
+            foreach (var item in basket.Items)
+            {
+                var productItem = await _unitOfWork.Repository<Core.Entities.Product>().GetByIdAsync(item.Id);
+                if (item.Price != productItem.Price)
+                {
+                    // If we're here then the client has manually tried to edit the price
+                    item.Price = productItem.Price;
+                }
+            }
+
+            // From stripe
+            var service = new PaymentIntentService();
+            PaymentIntent intent;
+
+            // If empty then we'll create a new intent
+            if (string.IsNullOrEmpty(basket.PaymentIntentId))
+            {
+                var options = new PaymentIntentCreateOptions
+                {
+                    // Amount is a long. To convert decimal into long, multiply it by 100
+                    Amount = (long)basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)shippingPrice * 100,
+                    Currency = "usd",
+                    PaymentMethodTypes = new List<string> { "card" }
+                };
+
+                // Creating our payment intent
+                intent = await service.CreateAsync(options);
+
+                // Updating our basket with the payment intent
+                basket.PaymentIntentId = intent.Id;
+                basket.ClientSecret = intent.ClientSecret;
+            }
+            else // We'll update the intent instead
+            {
+                var options = new PaymentIntentUpdateOptions
+                {
+                    Amount = (long)basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)shippingPrice * 100
+                };
+
+                await service.UpdateAsync(basket.PaymentIntentId, options);
+            }
+
+            // Updating the basket on the server
+            await _basketRepository.UpdateBasketAsync(basket);
+
+            return basket;
         }
     }
 }

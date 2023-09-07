1b0d71bde5f74979fe2f0955a267be2515855697
YoussefSS <joesamir98@gmail.com>
2023-09-07 11:30:14 +0300

    273. Updating the payments service

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -48,16 +48,18 @@ namespace API.Controllers
                 case "payment_intent.succeeded":
                     intent = (PaymentIntent)stripeEvent.Data.Object;
                     _logger.LogInformation("Payment succeeded: ", intent.Id);
-                    // TODO: update the order with the new status
+                    order = await _paymentService.UpdateOrderPaymentSucceeded(intent.Id);
+                    _logger.LogInformation("Order updated to payment received: ", order.Id);
                     break;
                 case "payment_intent.payment_failed":
                     intent = (PaymentIntent)stripeEvent.Data.Object;
                     _logger.LogInformation("Payment failed: ", intent.Id);
-                    // TODO: update the order with the new status
+                    order = await _paymentService.UpdateOrderPaymentFailed(intent.Id);
+                    _logger.LogInformation("Order updated to payment received: ", order.Id);
                     break;
             }
 
-            // Stripe expects this. We return this so that they stop trying to resend us the event.
+            // Stripe expects this. We return this so that stripe stops trying to resend us the event.
             return new EmptyResult();
         }
     }

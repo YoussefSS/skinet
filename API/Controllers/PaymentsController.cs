ae05ec8c09babc4d8a2dbf3a3629c3e6107bd40a
YoussefSS <joesamir98@gmail.com>
2023-09-07 11:17:55 +0300

    272. Webhooks

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -1,16 +1,21 @@
 using API.Errors;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace API.Controllers
 {
     public class PaymentsController : BaseApiController
     {
+        private const string WhSecret = ""; // Webhook secret
         private readonly IPaymentService _paymentService;
-        public PaymentsController(IPaymentService paymentService)
+        private readonly ILogger<PaymentsController> _logger;
+        public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
         {
+            _logger = logger;
             _paymentService = paymentService;
 
         }
@@ -24,6 +29,36 @@ namespace API.Controllers
             if (basket == null) return BadRequest(new ApiResponse(400, "Problem with your basket"));
 
             return basket;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            // Stripe will send the event details in the request body
+            var json = await new StreamReader(Request.Body).ReadToEndAsync();
+
+            // Using the WhSecret to verify that this event is actually coming from stripe
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], WhSecret);
+
+            PaymentIntent intent;
+            Order order; // from order aggregate
+
+            switch (stripeEvent.Type)
+            {
+                case "payment_intent.succeeded":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment succeeded: ", intent.Id);
+                    // TODO: update the order with the new status
+                    break;
+                case "payment_intent.payment_failed":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment failed: ", intent.Id);
+                    // TODO: update the order with the new status
+                    break;
+            }
+
+            // Stripe expects this. We return this so that they stop trying to resend us the event.
+            return new EmptyResult();
         }
     }
 }

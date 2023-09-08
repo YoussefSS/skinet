076f1bd9e3d5916fd23c4c944496d2288ff04b3a
YoussefSS <joesamir98@gmail.com>
2023-09-08 16:37:54 +0300

    291. Pre deployment work

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text.Json;
 using Core.Entities;
 using Core.Entities.OrderAggregate;
@@ -9,17 +10,19 @@ namespace Infrastructure.Data
         // Just having Task here means we're not returning anything, just updating our database
         public static async Task SeedAsync(StoreContext context)
         {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
             // Read JSON then Deserialze into our C# objects then add them to our Database
             if (!context.ProductBrands.Any()) // checks if there are any entries in our ProductBrands table
             {
-                var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json"); // this will be called from API
+                var brandsData = File.ReadAllText(path + @"/Data/SeedData/brands.json"); // this will be called from API
                 var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
                 context.ProductBrands.AddRange(brands);
             }
 
             if (!context.ProductTypes.Any())
             {
-                var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                var typesData = File.ReadAllText(path + @"/Data/SeedData/types.json");
                 var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
                 context.ProductTypes.AddRange(types);
             }
@@ -27,14 +30,14 @@ namespace Infrastructure.Data
             // Order is important, we need our ProductTypes and ProductBrands to be seeded before our Products
             if (!context.Products.Any())
             {
-                var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                var productsData = File.ReadAllText(path + @"/Data/SeedData/products.json");
                 var products = JsonSerializer.Deserialize<List<Product>>(productsData);
                 context.Products.AddRange(products);
             }
 
             if (!context.DeliveryMethods.Any())
             {
-                var deliveryData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+                var deliveryData = File.ReadAllText(path + @"/Data/SeedData/delivery.json");
                 var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(deliveryData);
                 context.DeliveryMethods.AddRange(methods);
             }
